801d8f66b3c3ff7248f14ef7dff0628813a30367
duaghwns <hj2758@gmail.com>
2022-09-11 21:44:42 +0900

    Hello Controller

--- src/main/java/com/example/restfulwebservice/controller/HelloController.java
+++ src/main/java/com/example/restfulwebservice/controller/HelloController.java
@@ -1,6 +1,7 @@
 package com.example.restfulwebservice.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -19,4 +20,16 @@ public class HelloController {
 
         return "Hello World";
     }
+
+    @GetMapping("hello-world-bean")
+    public HelloWorldBean helloWorldBean(){
+        return new HelloWorldBean("Hello world");
+    }
+
+    @GetMapping("path-variable/{name}")
+    public HelloWorldBean helloWorldBean(@PathVariable String name){
+        return new HelloWorldBean(String.format("Hello world, %s", name));
+    }
+
+
 }
