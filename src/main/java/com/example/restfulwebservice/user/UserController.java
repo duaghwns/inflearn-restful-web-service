34d8e0ff7c4e4e6142b27828491daae3546b62f7
duaghwns <hj2758@gmail.com>
2022-09-13 00:43:52 +0900

    user

--- src/main/java/com/example/restfulwebservice/user/UserController.java
+++ src/main/java/com/example/restfulwebservice/user/UserController.java
@@ -1,6 +1,7 @@
 package com.example.restfulwebservice.user;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -14,11 +15,15 @@ public class UserController {
         this.service = service;
     }
 
-    @GetMapping("/users")
+    @GetMapping("users")
     public List<User> retrieveAllUsers(){
         return service.findAll();
     }
 
+    @GetMapping("users/{id}")
+    public User retrieveUser(@PathVariable int id){
+        return service.findOne(id);
+    }
 
 
 }
