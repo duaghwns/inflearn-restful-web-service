baaeaea59dc2435b1a0b96a436746259bc50be0f
duaghwns <hj2758@gmail.com>
2022-10-04 23:00:42 +0900

    ㅇㅇ

--- src/main/java/com/example/restfulwebservice/user/User.java
+++ src/main/java/com/example/restfulwebservice/user/User.java
@@ -2,7 +2,6 @@ package com.example.restfulwebservice.user;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
-import lombok.NoArgsConstructor;
 
 import javax.validation.constraints.Past;
 import javax.validation.constraints.Size;
