0e9cc78050143230821c34580a4b80fbf699d224
duaghwns <hj2758@gmail.com>
2022-10-05 23:48:21 +0900

    test lotto

--- src/main/java/com/example/restfulwebservice/lotto/LottoController.java
+++ src/main/java/com/example/restfulwebservice/lotto/LottoController.java
@@ -1,9 +1,22 @@
 package com.example.restfulwebservice.lotto;
 
+import com.example.restfulwebservice.user.UserNotFoundException;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class LottoController {
 
-    public String
+    String lottoApiUrl = "https://www.dhlottery.co.kr/common.do?method=getLottoNumber&drwNo=";
+    @GetMapping("https://www.dhlottery.co.kr/common.do?method=getLottoNumber&drwNo={hoicha}")
+    public Lotto getLottoInfo(@PathVariable long hoicha){
+
+        Lotto lotto = service.findOne(hoicha);
+
+        if (lotto == null) {
+            throw new UserNotFoundException(String.format("Lotto[$s] not found", hoicha));
+        }
+        return lotto;
+    }
 }
