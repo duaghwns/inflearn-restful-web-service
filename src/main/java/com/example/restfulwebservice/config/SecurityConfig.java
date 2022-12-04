8d64f59068d7ba48c1ebdfc3b9c61d55f0f85adf
duaghwns <hj2758@gmail.com>
2022-12-04 20:31:49 +0900

    jpa h2 세팅

---
+++ src/main/java/com/example/restfulwebservice/config/SecurityConfig.java
@@ -0,0 +1,29 @@
+package com.example.restfulwebservice.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        // 이 경로로 들어오는 값은 다 허용
+        http.authorizeRequests().antMatchers("/h2-console/**").permitAll();
+        // csrf 사용 x
+        http.csrf().disable();
+        http.headers().frameOptions().disable();
+    }
+
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication()
+            .withUser("hojoon")
+            .password("{noop}test1234")
+            .roles("USER");
+
+    }
+}
