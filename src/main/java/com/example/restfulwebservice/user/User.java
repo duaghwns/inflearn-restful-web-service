ab9bf22d17fdd0fd357395afe9fb90e3122fbfdd
duaghwns <hj2758@gmail.com>
2022-12-04 18:58:33 +0900

    SWAGGER 커스텀

--- src/main/java/com/example/restfulwebservice/user/User.java
+++ src/main/java/com/example/restfulwebservice/user/User.java
@@ -1,6 +1,8 @@
 package com.example.restfulwebservice.user;
 
 import com.fasterxml.jackson.annotation.JsonFilter;
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -14,15 +16,23 @@ import java.util.Date;
 // @JsonIgnoreProperties(value = {"password"})
 //@JsonFilter("UserInfo")
 @NoArgsConstructor
+@ApiModel(description = "사용자 상세 정보를 위한 도메인 객체")
 public class User {
 
     private Integer id;
+
     @Size(min=2, message = "Name은 2글자 이상 입력해주세요.")
+    @ApiModelProperty(notes = "사용자 이름을 입력해 주세요.")
     private String name;
+
     @Past
+    @ApiModelProperty(notes = "등록일을 입력해 주세요.")
     private Date regDate;
 
+    @ApiModelProperty(notes = "비밀번호를 입력해 주세요.")
     private String password;
+
     // @JsonIgnore // Response 데이터 제어를 위한 필터링
+    @ApiModelProperty(notes = "주민번호를 입력해 주세요.")
     private String ssn;
 }
