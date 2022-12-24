fadb56bb81ccecea759d1f5217faf842035de0a0
duaghwns <hj2758@gmail.com>
2022-12-24 16:19:26 +0900

    jpa

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,3 +1,3 @@
-insert into user1 values (1, sysdate(), 'user1', 'test1111', '960416-1111111');
-insert into user1 values (2, sysdate(), 'user2', 'test2222', '960418-2222221');
-insert into user1 values (3, sysdate(), 'user3', 'test3333', '960417-3333333');
+insert into user1 values(1, '2022-12-09', 'user1','pass1','960000-000001');
+insert into user1 values(2, '2022-12-09', 'user1','pass1','960000-000001');
+insert into user1 values(3, '2022-12-09', 'user1','pass1','960000-000001');
